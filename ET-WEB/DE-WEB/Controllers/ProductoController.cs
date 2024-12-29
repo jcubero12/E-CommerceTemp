ed8de9fda6145aff130e40f3ebb98f119e31bce1
Jocksan Cubero García <jockcubero12@gmail.com>
2024-12-29 15:37:36 -0600

    Process Complete

--- ET-WEB/DE-WEB/Controllers/ProductoController.cs
+++ ET-WEB/DE-WEB/Controllers/ProductoController.cs
@@ -7,12 +7,16 @@ namespace DE_WEB.Controllers
         [HttpGet]
         public ActionResult ProductoDetail()
         {
+            ViewData["CurrentAction"] = this.ControllerContext.RouteData.Values["action"]?.ToString();
+            ViewData["CurrentController"] = this.ControllerContext.RouteData.Values["controller"]?.ToString();
             return View();
         }
 
         [HttpGet]
         public ActionResult Resenas()
         {
+            ViewData["CurrentAction"] = this.ControllerContext.RouteData.Values["action"]?.ToString();
+            ViewData["CurrentController"] = this.ControllerContext.RouteData.Values["controller"]?.ToString();
             return View();
         }
 
